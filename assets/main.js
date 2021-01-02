9a345cf72a71a53655257b9cb2340faf7f2ca577
Spitzy <spitzy.dev@gmail.com>
2021-01-02 04:02:03 -0500

    refresh stays on page

--- assets/main.js
+++ assets/main.js
@@ -7,15 +7,6 @@ $("#currentDay").text(date);
 var timeArray = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 var currentTime = moment().format("HH");
 
-//save my input
-$(".saveBtn").on("click", function()
-{
-    var hour = $(this).attr("id");
-    var input = $(this).siblings(".user-event-input").val();
-    localStorage.setItem(hour, input);
-
-});
-
 // colors
 $(".input-slot").each(function() {
     var block = parseInt($(this).prev().attr("data-hour"));
@@ -30,6 +21,23 @@ $(".input-slot").each(function() {
     }
  });
 
+//save my input
+$(".saveBtn").on("click", function()
+{
+    var hours = $(this).attr("id");
+    var input = $(this).siblings(".user-event-input").val();
+    localStorage.setItem(hours, input);
+
+});
+//refresh still there
+
+ function tasks () {
+    for (var i = 0; i < timeArray.length; i++) {
+        $(`#data-${timeArray[i]} .input-slot`).val(localStorage.getItem(timeArray[i]));
+    }
+}
+
+tasks();
 
 });
 

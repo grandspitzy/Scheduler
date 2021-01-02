75af07e03bec3592e907a05fe2692783caa9f775
Spitzy <spitzy.dev@gmail.com>
2021-01-02 03:37:02 -0500

    added time stuff and colors for time

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+
+//time 
+var date = moment().format("LLLL");
+$("#currentDay").text(date);
+var timeArray = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+var currentTime = moment().format("HH");
+
 $(".saveBtn").on("click", function()
 {
     var hour = $(this).attr("id");
@@ -5,3 +13,21 @@ $(".saveBtn").on("click", function()
     localStorage.setItem(hour, input);
 
 });
+
+// colors
+$(".input-slot").each(function() {
+    var block = parseInt($(this).prev().attr("data-hour"));
+        if (block == currentTime){
+        $(this).addClass("present");
+    }
+        else if (block < currentTime){
+        $(this).addClass("past");
+    }
+        else{
+        $(this).addClass("future");
+    }
+ });
+
+
+});
+
